5eec765370af9e4ac7536bb9744a79e2d38690f8
Joseph Colca <josephcolca@Josephs-Air.localdomain>
2018-09-18 14:49:11 -0500

    Added Total = text to printed result

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,6 +3,6 @@ tip = float(input("Please enter the tip percentage: "))
 
 def total_bill(sub_total, tip):
    total = sub_total + (sub_total * (tip / 100))
-   return float("{0:.2f}".format(total))
+   return f'Total = {float("{0:.2f}".format(total))}'
 
 print (total_bill(sub_total, tip))
